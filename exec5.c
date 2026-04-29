985aec2f93e541879d1f8ce2f9c4f2c46ca18f23
joaobatistademelosobrinho78-hash <joaobatistademelosobrinho77@gmail.com>
2026-04-29 20:36:34 -0300

    Create exec5.c

---
+++ exec5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int numero, total=0 , positivos = 0;
+    printf("informe o número;\n");
+    scanf("%d", &numero);
+    while (numero!= 0){
+        total++;
+        if(numero>0){
+            positivos++;
+            
+        }
+        printf("informe o número :\n");
+        scanf("%d", &numero);
+        
+    }
+    printf("Total: %d\n", total);
+    printf("Positivos: %d\n", positivos);
+    
+    return 0;
+}
